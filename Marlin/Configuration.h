0ec7378dac5fcc3916862945a2886abda85a913a
Craig Martin <me@craigmart.in>
2023-06-03 15:17:32 -0500

    Increase grid

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1548,7 +1548,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -45, -4.2, -0.72 } // CM
+#define NOZZLE_TO_PROBE_OFFSET { -45, -4.2, -0.74 } // CM
 
 // Enable and set to use a specific tool for probing. Disable to allow any tool.
 #define PROBING_TOOL 0
@@ -2071,7 +2071,7 @@
 #if EITHER(AUTO_BED_LEVELING_LINEAR, AUTO_BED_LEVELING_BILINEAR)
 
   // Set the number of grid points per dimension.
-  #define GRID_MAX_POINTS_X 3
+  #define GRID_MAX_POINTS_X 5 // CM
   #define GRID_MAX_POINTS_Y GRID_MAX_POINTS_X
 
   // Probe along the Y axis, advancing X after each column
